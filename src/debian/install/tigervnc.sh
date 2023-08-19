a0d2ed2ea9ea81119afea8ec982492f447e5c9d9
Tao Ma <tao@cxscore.ai>
2023-08-19 15:05:40 -0700

    use tightvnc because tighervnc doesnt listen on network for xclient

--- src/debian/install/tigervnc.sh
+++ src/debian/install/tigervnc.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Install TigerVNC server"
-apt-get install -y tigervnc-standalone-server
+#apt-get install -y tigervnc-standalone-server
+apt-get install -y tightvncserver
 apt-get clean -y
-printf '\n# docker-headless-vnc-container:\n$localhost = "no";\n1;\n' >>/etc/tigervnc/vncserver-config-defaults
+#printf '\n# docker-headless-vnc-container:\n$localhost = "no";\n1;\n' >>/etc/tigervnc/vncserver-config-defaults
